c0517be44744f506d4b33586313491e56f850520
tao.chai <tao.chai@thoughtworks.com>
2021-11-09 10:01:59 +0800

    TaoChai spring ioc homework problem3 using @Lazy

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +8,7 @@ public class Bar {
 
     private Foo foo;
 
+    @Lazy // why can not be used above Bar.class
     public Bar(Foo foo) {
         this.foo = foo;
     }
